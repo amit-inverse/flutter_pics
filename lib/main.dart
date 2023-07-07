a19a4dc65dc1e3ac17195d38c74dce19dad9f865
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-07 16:01:58 +0530

    Local Import Statements

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import './src/app.dart';
 
-void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Lets see some images!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-    ),
-  );
-
-  runApp(app);
+void main() {  
+  runApp(App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 
 // Create a class that will be our custom widget
 // This class must extend the 'StatelessWidget' base class
-
-// Must define a 'build' method that returns
-// the widgets that *this* widget will show
+class App extends StatelessWidget {
+  // Must define a 'build' method that returns
+  // the widgets that *this* widget will show
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Lets see some images!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            print('Hi there!');
+          },
+        ),
+      ),
+    );
+  }
+}
