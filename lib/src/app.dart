de57e2233c49b6752b339cd1d9c19fa743087e84
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-07 19:40:44 +0530

    Parsing Future Responses into a Model

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
+import 'dart:convert';
+import './models/image_model.dart';
 
 class App extends StatefulWidget {
   @override
@@ -11,10 +13,14 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
-  void fetchImage() {
+  void fetchImage() async {
     setState(() {
       counter += 1;
     });
+
+    var response = await get(
+        'https://jsonplaceholder.typicode.com/photos/$counter' as Uri);
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
   }
 
   @override
