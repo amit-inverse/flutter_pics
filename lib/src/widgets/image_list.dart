8f00716a9ee433e43545fac24c2956d15a2773d9
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-07 21:36:28 +0530

    Adding Border Style

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -12,6 +12,10 @@ class ImageList extends StatelessWidget {
       itemCount: images.length,
       itemBuilder: (context, int index) {
         return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          padding: const EdgeInsets.all(20.0),
           margin: const EdgeInsets.all(20.0),
           child: Image.network(images[index].url),
         );
