2f1146ee02d8d4fc02664e714c8dcac9d3461136
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-07 19:04:49 +0530

    Adding an Image Model

---
+++ lib/src/models/image_model.dart
@@ -0,0 +1,18 @@
+class ImageModel {
+  int id = 0;
+  String title = '';
+  String url = '';
+
+  ImageModel(this.id, this.url, this.title);
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    url = parsedJson['url'];
+    title = parsedJson['title'];
+  }
+
+  // ImageModel.fromJson(Map<String, dynamic> parsedJson)
+  //     : id = parsedJson['id'],
+  //       url = parsedJson['url'],
+  //       title = parsedJson['title'];
+}
